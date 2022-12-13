9df0ca73fbda1742b4200cf66171f72a43299dbb
root <angel3d3@gmail.com>
2022-12-13 16:01:46 -0400

    test module

---
+++ car_service/__manifest__.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Car Services",
+
+    'summary': """
+        Test module for Car Services""",
+
+    'description': """
+       This module Is a test for ODOO 
+    """,
+
+    'author': "angel3d3",
+    'website': "",
+
+    'category': 'Services',
+    'version': '0.1',
+    'sequence': 6,
+    'depends': ['base'],
+
+    # always loaded
+    'data': [
+        'views/backorder_report_menu.xml',
+    ],
+
+    'installable': True,
+}
